2838d6e29a6f02754bde816ac6f117b01ff9108b
keerthivasan283 <keerthiv272@gmail.com>
2025-08-19 20:08:59 +0530

    Add files via upload

---
+++ nested loop.java
@@ -0,0 +1,17 @@
+class nested {
+
+    public static void main(String[]args){
+
+        for(int i = 1; i<=3; i = i+1)
+        {
+            for( int j =1; j<=i; j = j+1)
+            {
+                
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
